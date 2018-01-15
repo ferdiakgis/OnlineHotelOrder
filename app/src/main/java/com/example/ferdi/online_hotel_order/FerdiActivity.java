ade5751a36339780f9b0b8d781c911ff8936ed86
ferdiakgis <ferdiakgis@gmail.com>
2018-01-15 11:06:42 +0300

    ferdiActivity

---
+++ app/src/main/java/com/example/ferdi/online_hotel_order/FerdiActivity.java
@@ -0,0 +1,13 @@
+package com.example.ferdi.online_hotel_order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FerdiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ferdi);
+    }
+}
